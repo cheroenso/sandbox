e83ba1f58cd3d1e8a178eab870d6dc31720a2f65
Christopher Brann <chris_brann@hotmail.com>
2014-01-03 06:30:31 -0800

    Initial checkin

---
+++ java/src/com/cwb/testing/StreamHelper.java
@@ -0,0 +1,72 @@
+package com.cwb.testing;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+
+
+public class StreamHelper {
+
+	private static String uriScheme = "http";
+	private static String httpAuth = "username:password";
+	
+	public void setUriScheme(String scheme){
+		uriScheme = scheme;
+	}
+	
+	public String getUriScheme(){
+		return uriScheme;
+	}
+	
+	public void setUserInfo(String userInfo){
+		httpAuth = userInfo;
+	}
+	
+	public String getUserInfo(){
+		return httpAuth;
+	}
+	
+	public static byte[] getBytesFromUriStream(String host, String port, String path, String query){
+		URI uri = null;
+		byte[] bArray = null;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		InputStream is = null;
+		String authority = host + ":" + port;
+		String fragment = "";
+		try {
+			uri = new URI(uriScheme, authority, path, query, fragment);
+			URL url = uri.toURL();
+			try {
+				is = url.openStream();
+			} catch (IOException ioe) {
+				System.err.println(ioe.getMessage());
+				System.err.println("request: " + url);
+			}
+			byte[] byteChunk = new byte[4096];
+			int i;
+			while (is != null && (i = is.read(byteChunk)) > 0){
+				baos.write(byteChunk, 0, i);
+			}
+			is.close();
+		} catch (Exception e){
+			e.getMessage();
+			System.err.println("ERROR: for stream request: " + uriScheme + authority + "/" + path + "?" + query + fragment);
+		} finally {
+			if (is != null){
+				try {
+				is.close();
+				} catch (IOException ioe){
+					System.err.println(ioe.getMessage());
+				}
+			}
+		}
+		if (baos != null) {
+			bArray = baos.toByteArray();
+		}
+		return bArray;
+		
+	}
+	
+}
